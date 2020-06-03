ca1aafbf92d24264c46f2db359af12a70820b4ac
Pete Balachin <pet.balachin1778@gmail.com>
2020-06-03 12:49:24 +0300

    Added Classes
    +Card
    +Deck

---
+++ Poker/Deck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Poker
+{
+    class Deck
+    {
+        private int Size = 52;
+        public List<Card> deck;
+        
+        public Deck()
+        {
+            foreach(Suit suit in Enum.GetValues(typeof(Suit)))
+            {
+                foreach(Rank rank in Enum.GetValues(typeof(Rank)))
+                {
+                    deck.Add(new Card(rank, suit));
+                }
+            }
+        }
+    }
+}
